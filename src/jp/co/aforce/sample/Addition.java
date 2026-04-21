94c0695f624d19e3faf288cab77be3c3fe611a6e
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-21 17:45:00 +0900

    Java演習第3章問2

---
+++ src/jp/co/aforce/sample/Addition.java
@@ -0,0 +1,11 @@
+package jp.co.aforce.sample;
+
+public class Addition {
+	public static void main (String[] args) {
+		int a = 8;
+		int b = 5;
+		int sum = a + b;
+		System.out.println(sum);
+	}
+
+}
