64b8c2d1d00c6ed33a40f8ae2f2ee8d352c1bbd5
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-21 17:39:27 +0900

    Java演習第3章問3

---
+++ src/jp/co/aforce/sample/Area.java
@@ -0,0 +1,11 @@
+package jp.co.aforce.sample;
+
+public class Area {
+	public static void main (String[] args) {
+		int width = 7;
+		int height = 4;
+		int area = width * height;
+		System.out.println(area);
+	}
+
+}
