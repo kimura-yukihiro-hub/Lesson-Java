210c34996b2c1cfcd43649b244955433006ee4d0
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-21 17:47:14 +0900

    Java演習第3章問4

---
+++ src/jp/co/aforce/sample/Widevarietyofdata.java
@@ -0,0 +1,13 @@
+package jp.co.aforce.sample;
+
+public class Widevarietyofdata {
+	public static void main (String[] args) {
+		String name = "Java";
+		double version = 17.0;
+		boolean isFun = true;
+		System.out.println(name);
+		System.out.println(version);
+		System.out.println(isFun);
+	}
+
+}
