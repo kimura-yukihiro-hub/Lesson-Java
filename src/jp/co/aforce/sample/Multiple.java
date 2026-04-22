e6baf17ee6247c5298390457056fa6766c412c2b
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-22 16:06:03 +0900

    Java演習第2章問9

---
+++ src/jp/co/aforce/sample/Multiple.java
@@ -0,0 +1,13 @@
+package jp.co.aforce.sample;
+
+public class Multiple {
+	public static void main (String[] args) {
+		int num = 15;
+		if (num % 5 == 0) {
+			System.out.println(num + "は5の倍数である。");
+		} else {
+			System.out.println(num + "は5の倍数ではない。");
+		}
+	}
+
+}
