c89c1f3d2b9119a477d2f67e44bcfc40d1272801
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-22 16:12:22 +0900

    Java演習第2章問15

---
+++ src/jp/co/aforce/sample/Average.java
@@ -0,0 +1,13 @@
+package jp.co.aforce.sample;
+
+public class Average {
+	public static void main (String[] args) {
+		int test1 = 75;
+		int test2 = 88;
+		int test3 = 92;
+		int sum = test1 + test2 + test3;
+		int Average = sum / 3;
+		System.out.println(Average);
+	}
+
+}
