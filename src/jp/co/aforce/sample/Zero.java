ec558f44e1d18d77e15767b36f0fb5a2d74f5fa6
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-22 16:00:41 +0900

    Java演習第2章問7

---
+++ src/jp/co/aforce/sample/Zero.java
@@ -0,0 +1,13 @@
+package jp.co.aforce.sample;
+
+public class Zero {
+	public static void main (String[] args) {
+		int num = 0;
+		if (num ==0) {
+			System.out.println("numは0です。");
+		} else {
+			System.out.println("numは0ではありません。");
+		}
+	}
+
+}
