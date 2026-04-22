35e1cf7208b9259d4b06a77a0d2c9a983eae627f
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-22 16:09:55 +0900

    Java演習第2章問11

---
+++ src/jp/co/aforce/sample/Difference.java
@@ -0,0 +1,10 @@
+package jp.co.aforce.sample;
+
+public class Difference {
+	public static void main (String[] args) {
+		int num1 = 10;
+		int num2 = 4;
+		System.out.println(num1 - num2);
+	}
+
+}
