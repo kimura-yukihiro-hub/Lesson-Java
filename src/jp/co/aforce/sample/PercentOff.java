899790f3b10d8c28d421125d52a1e197eccc9743
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-22 16:13:08 +0900

    Java演習第2章問16

---
+++ src/jp/co/aforce/sample/PercentOff.java
@@ -0,0 +1,10 @@
+package jp.co.aforce.sample;
+
+public class PercentOff {
+	public static void main (String[] args) {
+		int price = 12000;
+		double isPercentOff = price * 0.3;
+		System.out.println(price - isPercentOff + "円");
+	}
+
+}
