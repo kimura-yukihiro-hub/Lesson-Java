96396f472da7baa909f4df67815dcd8bcc225040
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-22 15:53:15 +0900

    Java演習第2章問2

---
+++ src/jp/co/aforce/sample/GreaterThanTen.java
@@ -0,0 +1,17 @@
+package jp.co.aforce.sample;
+
+public class GreaterThanTen {
+	public static void main (String[] args) {
+		int num = 12;
+		//比較演算の結果をboolean変数に代入
+		boolean isGreaterThanTen = num > 10;
+		//boolean変数の値を使って条件分岐
+		if (isGreaterThanTen) {
+			System.out.println(num + "は10より大きいです。");
+		} else {
+			System.out.println(num + "は10より小さいです。");
+		}
+		System.out.println("判定結果" + isGreaterThanTen);
+	}
+
+}
