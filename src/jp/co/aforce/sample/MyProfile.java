aa0db0562998d7de4ecef483c6b71bc5346214f0
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-22 16:10:47 +0900

    Java演習第2章問13

---
+++ src/jp/co/aforce/sample/MyProfile.java
@@ -0,0 +1,12 @@
+package jp.co.aforce.sample;
+
+public class MyProfile {
+	public static void main (String[] args) {
+		String name = "木村倖大";
+		int age = 24;
+		//年齢を次の歳に更新
+		age++;
+		System.out.println(name + "は次の誕生日で"+ age +"歳です。");
+	}
+
+}
