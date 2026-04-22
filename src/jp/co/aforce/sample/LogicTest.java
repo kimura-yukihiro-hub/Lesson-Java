acb17d0008eb8704a0f32cfb44d431665a0eef0f
kimura-yukihiro-hub <y.limura@a-force.co.jp>
2026-04-22 16:15:44 +0900

    Java演習第2章問20

---
+++ src/jp/co/aforce/sample/LogicTest.java
@@ -0,0 +1,15 @@
+package jp.co.aforce.sample;
+
+public class LogicTest {
+	public static void main(String[] args) {
+		int age = 20;
+		boolean hasStudentID = false;
+
+		if ((age < 25 && hasStudentID) || age == 20) {
+			System.out.println("割引対象です");
+		} else {
+			System.out.println("通常料金です");
+		}
+	}
+
+}
